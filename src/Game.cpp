b79861a04f18d753260bde8fcb99bfcb76173811
yjhpine <20211067@vision.hoseo.edu>
2022-09-28 16:51:16 +0900

    애니메이션스프라이트-실습2차
    1.애니메이션 동작

--- src/Game.cpp
+++ src/Game.cpp
@@ -46,7 +46,7 @@ bool Game::init(const char* title, int xpos, int ypos, int w, int h, int flags)
 }
 void Game::update()
 {
-	
+	m_sourceRectangle.x = 128 * ((SDL_GetTicks() / 100) % 6);
 }
 void Game::render()
 {
