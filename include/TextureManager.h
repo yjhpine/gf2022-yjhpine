771d63a6a7679ac6099073c72a91488c9778dd23
yjhpine <20211067@vision.hoseo.edu>
2022-10-14 13:24:49 +0900

    TextureManager-실습
    실습 완료했습니다..

--- include/TextureManager.h
+++ include/TextureManager.h
@@ -4,7 +4,6 @@
 #include <map>
 class TextureManager {
 public:
-
     TextureManager() {}
     ~TextureManager() {}
 
--- src/Game.cpp
+++ src/Game.cpp
@@ -29,36 +29,22 @@ bool Game::init(const char* title, int xpos, int ypos, int w, int h, int flags)
 	}
 
 
-	m_textureManager.load("Assets/Tree.png", "animate", m_pRenderer);
-	m_textureManager.load("Assets/CuteDog.png", "animate2", m_pRenderer);
-	m_textureManager.load("Assets/hihit.png", "animate3", m_pRenderer);
+	m_textureManager.load("Assets/animate-alpha.png", "animate", m_pRenderer);
 	
 	m_bRunning = true;
 	return true;
 }
 void Game::update()
 {
-	m_currentFrame = ((SDL_GetTicks() / 80) % 5);
+	m_currentFrame = ((SDL_GetTicks() / 100) % 6);
 }
 void Game::render()
 {
-	SDL_SetRenderDrawColor(m_pRenderer, 133, 251, 255, 255);//기본 배경
+	SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);//기본 배경
 	SDL_RenderClear(m_pRenderer);
-	
-	SDL_Rect fillRect = { 0, 400, 640, 200}; //흙바닥을 표현하는 사각형들
-	SDL_SetRenderDrawColor(m_pRenderer, 105, 73, 0, 255);
-	SDL_RenderFillRect(m_pRenderer, &fillRect);
 
-	SDL_Rect fillRect2 = { 0, 400, 640, 20 }; //잔디 표현
-	SDL_SetRenderDrawColor(m_pRenderer, 103, 255, 56, 255);
-	SDL_RenderFillRect(m_pRenderer, &fillRect2);
-
-	m_textureManager.draw("animate2", 250, 310, 128, 128, m_pRenderer);//강아지 스프라이트
-	m_textureManager.draw("animate", 180, 190, 256, 256, m_pRenderer);//나무 스프라이트
-	
-	m_textureManager.drawFrame("animate3", 400, 360, 64, 64, 0, m_currentFrame, m_pRenderer); //바람에 흩날리는 덤불..?을 표현하려 했습니다.
-	m_textureManager.drawFrame("animate3", 100, 360, 64, 64, 0, m_currentFrame, m_pRenderer); 
-	m_textureManager.drawFrame("animate3", 500, 360, 64, 64, 0, m_currentFrame, m_pRenderer);
+	m_textureManager.draw("animate", 0, 0, 128, 82, m_pRenderer);
+	m_textureManager.drawFrame("animate", 100, 100, 128, 82, 0, m_currentFrame, m_pRenderer);
 
 	SDL_RenderPresent(m_pRenderer);
 }
